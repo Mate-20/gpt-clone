adcd0e4e9ff2e9ba15460a0f23a4f221443c275d
Akash Jindal <20akash.jindal@gmail.com>
2025-09-14 18:22:17 +0530

    correcting modal height

--- src/components/ModalBlackScreen.tsx
+++ src/components/ModalBlackScreen.tsx
@@ -5,7 +5,7 @@ import { useModal } from '@/context/ModalBackgroundContext';
 const ModalBlackScreen = () => {
   const { isModalOpen } = useModal();
   return (
-    <div className={`${isModalOpen ? "h-[100vh] w-[100vw]" : "h-[0vh] w-[0vw]"} bg-[#00000080] absolute top-0 left-0 z-40`}></div>
+    <div className={`${isModalOpen ? "h-[94vh] w-[100vw]" : "h-[0vh] w-[0vw]"} bg-[#00000080] absolute top-0 left-0 z-40`}></div>
   )
 }
 
